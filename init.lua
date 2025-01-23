dd1fd616292ff3248fe2bd4ffe5105a9c4f40c12
Luciano Lim <luciano.lim@arcboats.com>
2025-01-22 21:53:17 -0800

    init

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local lspconfig = require("lspconfig")
+lspconfig.qmlls.setup({
+  cmd = { "pyside6-qmlls" },
+})
+lspconfig.tsserver.setup({
+  -- Basic settings
+  filetypes = { "typescript", "typescriptreact", "javascript" },
+  root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json"),
+})
+
+vim.opt.mouse = "a"
