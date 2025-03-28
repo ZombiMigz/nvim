edf51b5f95acb2480d72f92d6c80bb55e9a3719b
Luciano Lim <luciano.lim@arcboats.com>
2025-03-27 22:25:47 -0700

    updates

---
+++ lua/plugins/dart.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-flutter/flutter-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    ui = {
+      -- border = "rounded",
+    },
+    flutter_lookup_cmd = "asdf where flutter",
+    lsp = {
+      color = {
+        -- enabled = true,
+      },
+
+      -- https://github.com/dart-lang/sdk/blob/master/pkg/analysis_server/tool/lsp_spec/README.md#client-workspace-configuration
+      settings = {
+        lineLength = 120,
+        showTodos = false,
+
+        -- completeFunctionCalls = true,
+        -- renameFilesWithClasses = true,
+        analysisExcludedFolders = { "build", ".dart_tool", ".idea", ".pub", ".git" },
+      },
+    },
+  },
+}
