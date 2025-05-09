a9757795db5b24b1a9586f1599c395ee6702e182
Luciano Lim <luciano.lim@arcboats.com>
2025-05-09 11:34:29 -0700

    fix copy and paste and adds gha support

--- init.lua
+++ init.lua
@@ -5,14 +5,32 @@ local lspconfig = require("lspconfig")
 lspconfig.qmlls.setup({
   cmd = { "pyside6-qmlls" },
 })
+-- Github actions support, may be removed once Mason PR is merged here:
+-- https://github.com/lttb/gh-actions-language-server?tab=readme-ov-file
+vim.filetype.add({
+  pattern = {
+    [".*/%.github[%w/]+workflows[%w/]+.*%.ya?ml"] = "yaml.github",
+  },
+})
+require("lspconfig").gh_actions_ls.setup({})
 
 vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = false
 
+-- Use system clipboard by default
+vim.opt.clipboard = "unnamedplus"
+-- Enable yanking to system clipboard
 local osc52 = require("vim.ui.clipboard.osc52")
-vim.keymap.set("v", "y", '"+y', { noremap = true })
-vim.keymap.set("n", "p", '"+p', { noremap = true })
+vim.keymap.set("v", "y", function()
+  -- First yank to the unnamed register
+  vim.cmd("normal! y")
+  -- Now also yank to the system clipboard
+  local content = vim.fn.getreg('"')
+  vim.fn.setreg("+", content)
+  vim.fn.setreg("*", content)
+end, { noremap = true })
+-- Add OSC 52 support
 vim.g.clipboard = {
   name = "OSC 52",
   copy = {
@@ -21,10 +39,10 @@ vim.g.clipboard = {
   },
   paste = {
     ["+"] = function()
-      return { "" }
+      return { { "" }, "v" }
     end,
     ["*"] = function()
-      return { "" }
+      return { { "" }, "v" }
     end,
   },
 }
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    enabled = false,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      image = { enabled = true },
+      -- picker = { enabled = true, ui_select = true },
+      scroll = { enabled = true },
+      -- explorer = { enabled = true },
+      -- statuscolumn = { enabled = true },
+    },
+    -- keys = {
+    --   -- Top Pickers & Explorer
+    --   {
+    --     "<leader><space>",
+    --     function()
+    --       Snacks.picker.smart()
+    --     end,
+    --     desc = "Smart Find Files",
+    --   },
+    --   {
+    --     "<leader>/",
+    --     function()
+    --       Snacks.picker.grep()
+    --     end,
+    --     desc = "Find Text in Files",
+    --   },
+    --   {
+    --     "<leader>e",
+    --     function()
+    --       Snacks.explorer.open()
+    --     end,
+    --     desc = "Snacks Explorer",
+    --   },
+    -- },
+    dependencies = {
+      "kiyoon/magick.nvim",
+    },
+  },
+}
