d23cc4867c13fda9d4ee4992ebf5b1b2e8ac8479
Luciano Lim <luciano.lim@arcboats.com>
2025-06-23 23:55:57 -0700

    Moves to supermaven

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,29 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "<Esc>", function()
+  local blink = require("blink.cmp")
+
+  local suggestion_closed = false
+
+  -- Check if blink.cmp is visible
+  if blink.is_visible() then
+    blink.hide()
+    suggestion_closed = true
+  end
+
+  -- Check if supermaven has a suggestion and clear it
+  local supermaven = require("supermaven-nvim.completion_preview")
+  if supermaven.has_suggestion() then
+    supermaven.on_dispose_inlay()
+    suggestion_closed = true
+  end
+
+  if suggestion_closed then
+    return ""
+  end
+
+  -- If neither are active, normal escape behavior
+  return "<Esc>"
+end, { expr = true, silent = true })
---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    event = "InsertEnter",
+    cmd = {
+      "SupermavenUseFree",
+      "SupermavenUsePro",
+    },
+    opts = function()
+      require("supermaven-nvim.completion_preview").suggestion_group = "SupermavenSuggestion"
+      LazyVim.cmp.actions.ai_accept = function()
+        local suggestion = require("supermaven-nvim.completion_preview")
+        if suggestion.has_suggestion() then
+          LazyVim.create_undo()
+          vim.schedule(function()
+            suggestion.on_accept_suggestion()
+          end)
+          return true
+        end
+      end
+
+      return {
+        keymaps = {
+          accept_suggestion = "<Tab>", -- handled by nvim-cmp / blink.cmp
+        },
+        ignore_filetypes = { "bigfile", "snacks_input", "snacks_notif" },
+      }
+    end,
+  },
+}
