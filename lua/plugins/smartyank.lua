2da5c9591334afd18cb52ec0a6d9c38add756d9f
Luciano Lim <luciano.lim@arcboats.com>
2025-08-06 13:37:43 -0700

    Fixes yank outside of ssh and updates dependencies

--- lua/plugins/smartyank.lua
+++ lua/plugins/smartyank.lua
@@ -1,3 +1,11 @@
 return {
   "ibhagwan/smartyank.nvim",
+  config = function()
+    require("smartyank").setup({
+      osc52 = {
+        enabled = true,
+        ssh_only = false,
+      },
+    })
+  end,
 }
