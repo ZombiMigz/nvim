300f64c1f99fdcdffd9d1ba88ba21b0fc8097531
Luciano Lim <luciano.lim@arcboats.com>
2025-11-07 17:01:53 -0800

    Fixes picker to search root correctly

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -10,37 +10,33 @@ return {
     ---@type snacks.Config
     opts = {
       image = { enabled = true },
-      -- picker = { enabled = true, ui_select = true },
+      picker = {
+        enabled = true,
+        ui_select = true,
+      },
       scroll = { enabled = true },
       -- explorer = { enabled = true },
       -- statuscolumn = { enabled = true },
       notify = { enabled = true },
       notifier = { enabled = true },
     },
-    -- keys = {
-    --   -- Top Pickers & Explorer
-    --   {
-    --     "<leader><space>",
-    --     function()
-    --       Snacks.picker.smart()
-    --     end,
-    --     desc = "Smart Find Files",
-    --   },
-    --   {
-    --     "<leader>/",
-    --     function()
-    --       Snacks.picker.grep()
-    --     end,
-    --     desc = "Find Text in Files",
-    --   },
-    --   {
-    --     "<leader>e",
-    --     function()
-    --       Snacks.explorer.open()
-    --     end,
-    --     desc = "Snacks Explorer",
-    --   },
-    -- },
+    keys = {
+      -- Top Pickers & Explorer
+      {
+        "<leader><space>",
+        function()
+          Snacks.picker.smart({ cwd = vim.fn.getcwd(-1, -1) })
+        end,
+        desc = "Find Files (Root)",
+      },
+      {
+        "<leader>/",
+        function()
+          Snacks.picker.grep({ cwd = vim.fn.getcwd(-1, -1) })
+        end,
+        desc = "Grep (Root)",
+      },
+    },
     dependencies = {
       "kiyoon/magick.nvim",
     },
