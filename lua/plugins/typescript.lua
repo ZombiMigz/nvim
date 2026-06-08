4d4d08e73d94edd27dcb3ef48d2b2a86eba0c6d3
Luciano Lim <luciano.lim@arcboats.com>
2026-06-08 13:42:22 -0700

    Update mason pointer

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,19 +1,43 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      vtsls = {
-        capabilities = {
-          workspace = {
-            didChangeWatchedFiles = {
-              dynamicRegistration = true,
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "vue-language-server", "vtsls" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        vue_ls = {},
+        vtsls = {
+          filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+          capabilities = {
+            workspace = {
+              didChangeWatchedFiles = {
+                dynamicRegistration = true,
+              },
             },
           },
-        },
-        settings = {
-          typescript = {
-            tsserver = {
-              maxTsServerMemory = 6144,
+          settings = {
+            typescript = {
+              tsserver = {
+                maxTsServerMemory = 6144,
+              },
+            },
+            vtsls = {
+              tsserver = {
+                globalPlugins = {
+                  {
+                    name = "@vue/typescript-plugin",
+                    location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
+                    languages = { "vue" },
+                    configNamespace = "typescript",
+                    enableForWorkspaceTypeScriptVersions = true,
+                  },
+                },
+              },
             },
           },
         },
